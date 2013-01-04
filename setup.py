8143462e70d25797fd0557c0f13e6bc87e36cd99
Michael.Imelfort <mike@mikeimelfort.com>
2013-01-04 12:18:49 +1000

    Fixed after renaming

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bam2fpkc',
-    version='0.0.1',
+    version='0.1.0',
     author='Michael Imelfort',
     author_email='mike@mikeimelfort.com',
     packages=['bam2fpkc', 'bam2fpkc.test'],
@@ -11,5 +11,8 @@ setup(
     license='LICENSE.txt',
     description='bam2fpkc',
     long_description=open('README.txt').read(),
-    install_requires=[],
+    install_requires=[
+        "pysam >= 0.6",
+        "numpy >= 1.6.1",
+    ],
 )
